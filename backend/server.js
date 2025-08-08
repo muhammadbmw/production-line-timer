12afcca506842a9e262a1313eb81a1ec0b28d358
Muhammad Mahbubur Rahman <muhammad@breezemaxweb.com>
2025-08-08 10:46:54 -0400

    Connected to mongodb server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(5000, ()  => {
-    console.log('Server started on port: 5000');
+    connectDB();
+    console.log(`Server started on port: ${PORT}`);
 })
